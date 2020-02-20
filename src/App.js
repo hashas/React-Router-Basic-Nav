c77fe751a67b895b7371c9cf2f31762a6038c59f
hh <hh@hhmbp15.local>
2020-02-20 14:59:19 +0000

    corrected Route compoments and declared Link components inside Navigation.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { Home, About, Contact, Navigation } from './components';
 
 import { Route } from 'react-router-dom';
 
-<Route exact path='/' component={Home}/>
-<Route path='/about' component={About}/>
-<Route path='/contact' component={Contact}/>
-
-
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path='/' component={Home}/>
+    <Route path='/about' component={About}/>
+    <Route path='/contact' component={Contact}/>
   </div>
 );
 
